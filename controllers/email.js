8afb9717c44ad52ff89376d3f04bf86ec6901a6a
Breogan Gonzalez <breogan.gonzalez@getsetpay.com>
2015-10-03 20:52:21 +0200

    Render email using html template

--- controllers/email.js
+++ controllers/email.js
@@ -1,32 +1,53 @@
 var nodemailer = require('nodemailer');
 
+var EmailTemplate = require('email-templates').EmailTemplate
+var path = require('path')
+var templateDir = path.join(__dirname, '../templates', 'welcome-email');
+
 exports.sendMail = function(req, res){
     
+    var name = req.body.name;
     var subject = req.body.subject;
     var from = req.body.from;
     var to = req.body.to;
-    var message = req.body.message;
 
-    var smtpTransport = nodemailer.createTransport({
+    var newsletter = new EmailTemplate(templateDir);
+
+    var user = {
+        name: name
+    };
+
+        var smtpTransport = nodemailer.createTransport({
         service: "Gmail",
         auth: {
             user: "xxxx@gmail.com",
             pass: "xxxx"
         }
     });
-    var mailOptions = {
-        subject: subject,
-        from: from,
-        to: to, 
-        html: message
-    }
-    smtpTransport.sendMail(mailOptions, function(error, response){
-            if(error){
-            console.log(error);
-            return res.send(500, error);
-        }else{
-            res.status(201).end();
-        }
 
+    newsletter.render(user, function (err, results) {
+        if(err){
+            console.log(err);
+        }
+        else{
+            htmlResult = results.html;
+
+            var mailOptions = {
+                    subject: subject,
+                    from: from,
+                    to: to, 
+                    html: htmlResult
+                };
+            
+            smtpTransport.sendMail(mailOptions, function(error, response){
+                if(error){
+                console.log(error);
+                return res.send(500, error);
+                }else{
+                    res.status(201).end();
+                }
+
+            });
+        }
     });
 }
--- test/emailTest.js
+++ test/emailTest.js
@@ -47,10 +47,10 @@ describe('Email Test', function() {
                 .post('/api/email')
                 .set('Authorization', 'Basic YnJlb2dhbmdmOkJyZW9nYW4yMDE1')
                 .send({
+                    "name": "Breogan",
                     "subject": "Welcome to myFuelCloud",
                     "from": "support@5labs.com",
                     "to": "breogangf@gmail.com",
-                    "message": "<h1><b>We're glad you fund us!</b></h1><p><h2>Welcome to myFuelCloud. Here are a few things we love (and hope you love to):</h2></p><ol type=\"1\"><li><h3>We're 100% funded by our customers. We <3 you guys.</h3></li><li><h3>This is an <a href=\"https://github.com/breogangf/myFuelCloud/\">opensource</a> project, you can help make this app even better!</h3></li><li><h3>We make lots of cool stuff, you can take a look at <a href=\"http://5labs.com/\">5labs.com</a></h3></li></ol><br><h3>We're happy to have you onboard and we'd love to hear from you!</h3> <h3>Happy fueling,</h3><br><h3>Breogán González</h3>"
                 })
                 .expect(201)
                 .end(function(err, res) {
